ecfeee880858e74138c9c10a55cdcc35bde0f150
smallPterodactyl <119345529+smallPterodactyl@users.noreply.github.com>
2023-03-03 11:34:17 +0000

    feat: Add base router component

--- client/src/routes/fl-justice-router.tsx
+++ client/src/routes/fl-justice-router.tsx
@@ -1,5 +1,7 @@
 
+import { NOTFOUND } from "dns";
 import { BrowserRouter, Route } from "react-router-dom";
+import Confess from "../components/confess/confess";
 import Home from "../components/home/home";
 import Misdemeanours from "../components/misdemeanours/misdemeanours";
 
@@ -9,9 +11,10 @@ export const JusticeRouter : React.FC = () =>
     <>
         <BrowserRouter>
             <Route path="/" element={<Home/>} />
-            <Route path="/misdemeanours" element={<Misdemeanours/>} /> 
+            <Route path="/misdemeanours" element={<Misdemeanours/>} />
+            <Route path="/confess" element={<Confess/>} /> 
+            <Route path="*" element={<NOTFOUND/>}/>
         </BrowserRouter>
-
     </>
  
 
