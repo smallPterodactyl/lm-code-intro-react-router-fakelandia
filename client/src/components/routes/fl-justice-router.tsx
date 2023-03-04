5eb4f40553b7844cc8003bfc1f3774be0dff9e54
smallPterodactyl <119345529+smallPterodactyl@users.noreply.github.com>
2023-03-04 10:07:39 +0000

    route: nest routes in layout route

--- client/src/components/routes/fl-justice-router.tsx
+++ client/src/components/routes/fl-justice-router.tsx
@@ -5,14 +5,18 @@ import Home from "../home/home";
 import Misdemeanours from "../misdemeanours/misdemeanours";
 import Confess from "../confess/confess"
 import NotFound from "../notfound/notfound"
+import MainLayout from "../layout/layout";
 
 export const JusticeRouter : React.FC = () => 
 
     <>
         <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/misdemeanours" element={<Misdemeanours/>} />
-            <Route path="/confess" element={<Confess/>} /> 
+            <Route path='/' element = {<MainLayout/>}>
+                <Route index element={<Home/>} />
+                <Route path= 'misdemeanours' element={<Misdemeanours/>} />
+                <Route path= 'confess' element={<Confess/>} /> 
+            </Route>
+
             <Route path="*" element={<NotFound/>}/>
         </Routes>
     </>
